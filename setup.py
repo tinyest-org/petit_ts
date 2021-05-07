807255d1133364d58c34df29a99de67a1886edd1
Paul Leveau <plawn.yay@gmail.com>
2021-05-07 14:48:24 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('readme.md', 'r') as f :
 
 setup(
     name='petit_ts',
-    version='0.2.0',
+    version='0.2.1',
     description='Convert your python types to typescript',
     packages=['petit_ts'],
     url='https://github.com/Plawn/petit_ts',
